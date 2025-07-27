d1d925087c49076fcd142ec78391daad821f3df1
Anna Baskakova <146026108+Hannaary@users.noreply.github.com>
2025-07-28 05:33:33 +0700

    init

---
+++ src/test/java/com/example/securetoken/SecureTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securetoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
